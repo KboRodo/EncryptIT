831b28f03595cf4e42fcd8a2e0ac6d05dbf30ac0
KboRodo <61941560+KboRodo@users.noreply.github.com>
2020-10-16 19:40:46 +0200

    dodano szyfrowanie vigenere

--- encryptors/vigenere.js
+++ encryptors/vigenere.js
@@ -1,5 +1,5 @@
 const button=document.getElementById('vigenere');
-const moveBy=document.querySelector('#keyword')
+const keyword=document.querySelector('#keyword');
 button.addEventListener('click',encryptCeasar);
 
 
@@ -8,22 +8,48 @@ function encryptCeasar(){//zaszyferowanie tekstu według klucza
     const inputText=document.getElementById('input-text').value;
     const output=document.getElementById('output-text');
     const inputArray=inputText.split('')
-    let index
     let outputText=[]
+    let matchedKeywordArray=keyword.value.split('')
+    const keywordLength=matchedKeywordArray.length
+    let col
+    let row
 
-    const encryptionKey=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
-    const alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']//referencja alfabetu
-
-    for(let i=0; i<move; i++){
-        encryptionKey.unshift(encryptionKey[encryptionKey.length-1])
-        encryptionKey.pop()
+    //TWORZENIE TABLICY 26X26 Z ALFABETEM
+    const alphabetArray= new Array(26)
+    for(let i=0; i<26; i++){
+        alphabetArray[i]= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+        for(let mv=0; mv<i; mv++){
+            alphabetArray[i].push(alphabetArray[i][0])
+            alphabetArray[i].shift()
+        }
     }
 
-    inputArray.forEach(element => {
-        (element===' ') ? 
-        outputText.push(' '):
-        index=alphabet.indexOf(element)
-        outputText.push(encryptionKey[index])
-    });
-    output.innerHTML=outputText.join('')//usinac przecinki
+    //PRZYGOTOWANIE SŁOWA KLUCZA
+    while(matchedKeywordArray.length<inputArray.length){
+        for(let i=0; i<keywordLength; i++){
+            matchedKeywordArray.push(matchedKeywordArray[i])
+        }
+    }
+
+    //OBSLUGA SPACJI
+    for(let i=0; i<inputArray.length; i++){
+        if(inputArray[i]===" "){
+            matchedKeywordArray[i]=" "
+        }
+    }
+
+    //DŁUGOSC KLUCZA=DLUGOSC INPUTU
+    while(matchedKeywordArray.length>inputArray.length){
+        matchedKeywordArray.pop()
+    }
+
+    //SZYFROWANIE TEKSTU
+    for(let i=0; i<inputArray.length; i++){
+        row=alphabetArray[0].indexOf(inputArray[i])
+        col=alphabetArray[0].indexOf(matchedKeywordArray[i])
+        outputText+=alphabetArray[row][col]
+    }
+    console.log(outputText)
+    //WYPISANIE ZASZYFROWANEGO TEKSTU
+    output.innerHTML=outputText
 }
