cb70b8d0004322d8301cafe61eb610587367e08f
KboRodo <61941560+KboRodo@users.noreply.github.com>
2020-10-19 20:43:14 +0200

    dodano odszyfrowywanie vigenera

--- encryptors/vigenere.js
+++ encryptors/vigenere.js
@@ -1,20 +1,52 @@
 const button=document.getElementById('vigenere');
 const keyword=document.querySelector('#keyword');
-button.addEventListener('click',encryptCeasar);
+
+const modeEncrypt=document.querySelector('#encrypt')//tryb szyfrowania
+const modeDecrypt=document.querySelector('#decrypt')//tryb odszyfrowywania
+
+//OBSŁUGA OPCJI SZYFROWANIA/ODSZYFROWANIA
+modeEncrypt.addEventListener('click',function(){
+    button.removeEventListener('click',decryptText)
+    button.addEventListener('click',encryptText)
+    modeDecrypt.classList.remove('active')
+    modeEncrypt.classList.add('active')
+})
+modeDecrypt.addEventListener('click',function(){
+    button.removeEventListener('click',encryptText)
+    button.addEventListener('click',decryptText)
+    modeDecrypt.classList.add('active')
+    modeEncrypt.classList.remove('active')
+})
+modeEncrypt.click()
 
 
-function encryptCeasar(){//zaszyferowanie tekstu według klucza
-    const move=moveBy.value
+function encryptText(){//zaszyferowanie tekstu według klucza
+    const inputArray=readName()
+    const alphabetArray=createEncryptorArray()
+    const keyword=createKeyword(inputArray)
+    const outputText=encryptVigenere(keyword,inputArray,alphabetArray)
+    writeText(outputText)
+}
+
+function decryptText() {
+    const inputArray=readName()
+    const alphabetArray=createEncryptorArray()
+    const keyword=createKeyword(inputArray)
+    const outputText=decryptVigenere(keyword,inputArray,alphabetArray)
+    writeText(outputText)
+}
+
+function readName() {
     const inputText=document.getElementById('input-text').value;
-    const output=document.getElementById('output-text');
     const inputArray=inputText.split('')
-    let outputText=[]
-    let matchedKeywordArray=keyword.value.split('')
-    const keywordLength=matchedKeywordArray.length
-    let col
-    let row
+    return inputArray}
 
-    //TWORZENIE TABLICY 26X26 Z ALFABETEM
+function writeText(outputText) {
+    const output=document.getElementById('output-text')
+    output.innerHTML=outputText
+}
+
+function createEncryptorArray() {
     const alphabetArray= new Array(26)
     for(let i=0; i<26; i++){
         alphabetArray[i]= ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
@@ -23,33 +55,74 @@ function encryptCeasar(){//zaszyferowanie tekstu według klucza
             alphabetArray[i].shift()
         }
     }
+    return alphabetArray
+}
 
-    //PRZYGOTOWANIE SŁOWA KLUCZA
+function createKeyword(inputArray) {
+    const keyword=document.querySelector('#keyword')
+    let matchedKeywordArray=keyword.value.split('')
+    const keywordLength=matchedKeywordArray.length
+
     while(matchedKeywordArray.length<inputArray.length){
         for(let i=0; i<keywordLength; i++){
+            if(inputArray[i]===" "){
+                matchedKeywordArray.push(" ")
+            }
+            else{
             matchedKeywordArray.push(matchedKeywordArray[i])
+            }
         }
     }
 
-    //OBSLUGA SPACJI
     for(let i=0; i<inputArray.length; i++){
         if(inputArray[i]===" "){
             matchedKeywordArray[i]=" "
         }
     }
-
-    //DŁUGOSC KLUCZA=DLUGOSC INPUTU
     while(matchedKeywordArray.length>inputArray.length){
         matchedKeywordArray.pop()
     }
 
-    //SZYFROWANIE TEKSTU
+    return matchedKeywordArray
+}
+
+function encryptVigenere(matchedKeywordArray, inputArray, alphabetArray) {
+    let outputText=''
+    let row
+    let col
     for(let i=0; i<inputArray.length; i++){
-        row=alphabetArray[0].indexOf(inputArray[i])
-        col=alphabetArray[0].indexOf(matchedKeywordArray[i])
-        outputText+=alphabetArray[row][col]
+        if(matchedKeywordArray[i]===" ")
+        {
+            outputText+=" "
+        }
+        else{
+            row=alphabetArray[0].indexOf(inputArray[i])
+            col=alphabetArray[0].indexOf(matchedKeywordArray[i])
+            outputText+=alphabetArray[row][col]
+        }
     }
-    console.log(outputText)
-    //WYPISANIE ZASZYFROWANEGO TEKSTU
-    output.innerHTML=outputText
+    return outputText
+}
+
+function decryptVigenere(matchedKeywordArray, inputArray, alphabetArray) {
+    let outputText=''
+    let row
+    let col
+    for(let i=0; i<inputArray.length; i++){
+        if(matchedKeywordArray[i]===" ")
+        {
+            outputText+=" "
+        }
+        else{
+            col=alphabetArray[0].indexOf(matchedKeywordArray[i])
+            for(let j=0; j<26; j++){
+                if(alphabetArray[j][col]===inputArray[i]){
+                    row=j
+                }
+            }
+            console.log(row,col)
+            outputText+=alphabetArray[row][0]
+        }
+    }
+    return outputText
 }
